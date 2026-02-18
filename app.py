3e321a22391e23ef2af5bd4db9933d0ad412aa5d
shanthiS15 <23d151@psgitech.ac.in>
2026-02-18 11:28:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("Simple Calculator 🧮")
+
+num1 = st.number_input("Enter First Number:", value=0.0)
+num2 = st.number_input("Enter Second Number:", value=0.0)
+
+operation = st.selectbox(
+    "Select Operation:",
+    ("Addition", "Subtraction", "Multiplication", "Division")
+)
+
+if st.button("Calculate"):
+    if operation == "Addition":
+        result = num1 + num2
+        st.success(f"Result: {result}")
+
+    elif operation == "Subtraction":
+        result = num1 - num2
+        st.success(f"Result: {result}")
+
+    elif operation == "Multiplication":
+        result = num1 * num2
+        st.success(f"Result: {result}")
+
+    elif operation == "Division":
+        if num2 != 0:
+            result = num1 / num2
+            st.success(f"Result: {result}")
+        else:
+            st.error("Division by zero is not allowed ❌")
